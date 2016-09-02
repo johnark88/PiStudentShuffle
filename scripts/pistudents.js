e605dc0e5b4880f56b811da483b651baddff4d53
John Arkema <john.arkema@gmail.com>
2016-09-02 07:54:52 -0500

    Getting json Data

--- scripts/pistudents.js
+++ scripts/pistudents.js
@@ -1,1 +1,28 @@
 console.log('Yes pistudents is sourced');
+
+
+var students;
+
+$(document).ready(function(){
+  console.log('In doc ready ');
+
+  var searchURL = 'http://devjana.net/pi/pi_students.json';
+  console.log(searchURL);
+
+  //ajax call
+  $.ajax({
+    url: searchURL,
+    dataType: 'JSON',
+    success: function(data) {
+      console.log('get json data = ', data);
+      students = data;
+      console.log(students, 'work');
+    showStudents(data);
+    }
+  });
+});
+
+var showStudents = function(){
+  console.log('showStudents, ' , students);
+  $('#outputDiv').empty();
+};
