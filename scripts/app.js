d86df3b1080bc0149e2ec465de6e6be9a1c842c8
John Arkema <john.arkema@gmail.com>
2016-09-03 17:39:15 -0500

    NOT 1 at a time

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,58 @@
-console.log('Yes pistudents is sourced');
+console.log('Yes app.js is sourced');
 
 
-var students = [];
-var counter = 0;
+
 
 $(document).ready(function(){
+  var students = [];
+  var counter = 0;
   console.log('In doc ready ');
-
+  //URL to retrive json data from
   var studentsURL = 'http://devjana.net/pi/pi_students.json';
-  console.log(studentsURL);
+  // console.log(studentsURL);
 
   //ajax call
   $.ajax({
     url: studentsURL,
     dataType: 'JSON',
     success: function(data) {
-      console.log('great success = ', data.students);
+      // console.log('great success = ', data.students);
       for (var i = 0; i < data.students.length; i++) {
+        //push to global array
       students.push(data.students[i]);
       }
-
-    showStudents();
   }//end success
   });//end ajax call
+
+
+// $( '.nextButton' ).on( 'click' , function(){
+//   console.log('showStudents, ' , students);
+//   //clear the output div
+//   var outputDiv = $('#outputDiv');
+//   outputDiv.empty();
+//   //loop through the array of objects and display on DOM
+//   for (var i = 0; i < students.length; i++) {
+//   //create header for student name
+//   var nameHeader = document.createElement('h2');
+//   //get name for header
+//   nameHeader.textContent = students[i].first_name + ' ' + students[i].last_name;
+//   //create new paragraph element for student info
+//   var infoPara = document.createElement('p');
+//   //get info for paragraph
+//   infoPara.textContent = students[i].info;
+//
+//   //append to the DOM for display
+//   outputDiv.append(nameHeader);
+//   outputDiv.append(infoPara);
+// }//end for loop
+// });//next button click
+
+
+
+$('.nextButton').on('click', function(){
+$('#outputDiv').empty();
+for (var i = 0; i < students.length; i++) {
+  $('#outputDiv').append('<p>'+ students[i].first_name + ' ' + students[i].last_name);
+}  students[i]++;
+});
 });//end document ready
-
-var showStudents = function(){
-  console.log('showStudents, ' , students);
-  //clear the output div
-  var outputDiv = $('#outputDiv');
-  outputDiv.empty();
-
-
-  var nameHeader = document.createElement('h2');
-  var newPara = document.createElement('p');
-
-  //loop through the array of objects and display on DOM
-  for (var i = 0; i < students.length; i++) {
-
-  // $( '.nextButton' ).on( 'click' , function(){
-    // counter++;
-
-
-    // outputDiv.append('<p>' + 'Name: ' +' ' + students[i].first_name + ' ' + students[i].last_name + '</p>');
-    // outputDiv.append('<p>' + 'blurb: '+ ' ' + students[i].info + '</p>');
-
-
-// console.log('counter = ', counter);
-
-  // });
-}
-};
