643920f7eb49e0c13932909b33e3c9c372739619
John Arkema <john.arkema@gmail.com>
2016-09-04 09:34:06 -0500

    button div's

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 console.log('Yes app.js is sourced');
-
-
+// global variables
+var students = [];
+var count = 0;
 
 
 $(document).ready(function(){
-  var students = [];
-  var counter = 0;
   console.log('In doc ready ');
+
   //URL to retrive json data from
   var studentsURL = 'http://devjana.net/pi/pi_students.json';
   // console.log(studentsURL);
@@ -16,43 +16,35 @@ $(document).ready(function(){
     url: studentsURL,
     dataType: 'JSON',
     success: function(data) {
-      // console.log('great success = ', data.students);
+      console.log('great success = ', data.students);
       for (var i = 0; i < data.students.length; i++) {
         //push to global array
-      students.push(data.students[i]);
+        students.push(data.students[i]);
+        showStudents();
       }
-  }//end success
+    }//end success
   });//end ajax call
 
+  var showStudents = function(){
+    // console.log('showStudents, ' , students);
+    //clear the output div
+    var outputDiv = $('#outputDiv');
+    outputDiv.empty();
+    //loop through the array of objects and display on DOM
+    for (var i = 0; i < students.length; i++) {
+      //create header for student name
+      var nameHeader = document.createElement('h2');
+      //get name for header
+      nameHeader.textContent = students[i].first_name + ' ' + students[i].last_name;
+      //create new paragraph element for student info
+      var infoPara = document.createElement('p');
+      //get info for paragraph
+      infoPara.textContent = students[i].info;
 
-// $( '.nextButton' ).on( 'click' , function(){
-//   console.log('showStudents, ' , students);
-//   //clear the output div
-//   var outputDiv = $('#outputDiv');
-//   outputDiv.empty();
-//   //loop through the array of objects and display on DOM
-//   for (var i = 0; i < students.length; i++) {
-//   //create header for student name
-//   var nameHeader = document.createElement('h2');
-//   //get name for header
-//   nameHeader.textContent = students[i].first_name + ' ' + students[i].last_name;
-//   //create new paragraph element for student info
-//   var infoPara = document.createElement('p');
-//   //get info for paragraph
-//   infoPara.textContent = students[i].info;
-//
-//   //append to the DOM for display
-//   outputDiv.append(nameHeader);
-//   outputDiv.append(infoPara);
-// }//end for loop
-// });//next button click
+      //append to the DOM for display
+      outputDiv.append(nameHeader);
+      outputDiv.append(infoPara);
+    }//end for loop
+  };
 
-
-
-$('.nextButton').on('click', function(){
-$('#outputDiv').empty();
-for (var i = 0; i < students.length; i++) {
-  $('#outputDiv').append('<p>'+ students[i].first_name + ' ' + students[i].last_name);
-}  students[i]++;
-});
 });//end document ready
